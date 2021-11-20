66e4f82a1a2f414b0e12714046a632895603bc43
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2021-11-20 21:24:26 +0100

    Fist, default constructible version
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>

--- include/myoptional.h
+++ include/myoptional.h
@@ -1,11 +1,60 @@
 #ifndef MYOPTIONAL_H
 #define MYOPTIONAL_H
 
-enum class Status {
-    OK,
-    INVALID_ARG,
-    ERROR
+#include <iostream>
+#include <type_traits>
+
+enum class Status { OK = 0, INVALID_ARG, ERROR };
+
+template <class T, std::enable_if_t<std::is_class<T>::value, bool> = true>
+class myoptional {
+public:
+    // Empty object
+    constexpr myoptional() = delete;
+
+    // Constructor assigning value
+    template <class P, std::enable_if_t<std::is_same<std::remove_const_t<T>, P>::value, bool> = true>
+    constexpr myoptional(const P& val)
+        // constexpr myoptional(const T& val)
+        : _value(val)
+        , _status(Status::OK)
+    {
+        std::cout << "User!" << std::endl;
+    }
+
+    // Constructor assigning error code
+    template <class P, std::enable_if_t<std::is_same<P, Status>::value, bool> = true>
+    constexpr myoptional(const P& val)
+    {
+        std::cout << "Status!" << std::endl;
+        // static_assert(PVal == Status::OK, "dupaaaa!");
+        // std::cout << "const " << std::is_const<T>::value << std::endl;
+    }
+
+    constexpr const T* operator->() const noexcept
+    {
+        return operator bool() ? &_value : nullptr;
+    }
+
+    constexpr T* operator->() noexcept
+    {
+        return operator bool() ? &_value : nullptr;
+    }
+
+    constexpr explicit operator bool() const noexcept
+    {
+        return status() == Status::OK;
+    }
+
+    constexpr Status status() const noexcept
+    {
+        return _status;
+    }
+
+private:
+    T _value;
+    Status _status{ Status::INVALID_ARG };
 };
 
+#endif // MYOPTIONAL_H
 
-#endif /* !MYOPTIONAL_H */
--- test/myoptional_test.cpp
+++ test/myoptional_test.cpp
@@ -1,10 +1,23 @@
 #include <myoptional.h>
+
 #include <cstdlib>
 
-int main(int argc, char** argv)
-{
+class DefaultClass {
+public:
+    int abc;
+};
+
+myoptional<DefaultClass> func1() { return DefaultClass(); }
+
+myoptional<DefaultClass> func2() { return Status::ERROR; }
+
+int main(int argc, char** argv) {
+    myoptional<const DefaultClass> op1 = DefaultClass();
+    myoptional<DefaultClass> op2 = Status::ERROR;
+
+    myoptional<DefaultClass> op3 = func1();
+    myoptional<DefaultClass> op4 = func2();
+
     return EXIT_SUCCESS;
 }
-
-
 
