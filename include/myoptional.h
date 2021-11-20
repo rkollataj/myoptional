d9b872978ecb6916f3c0e67e67fd873681347dab
Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>
2021-11-20 15:36:03 +0100

    Initial version
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@gmail.com>

---
+++ include/myoptional.h
@@ -0,0 +1,11 @@
+#ifndef MYOPTIONAL_H
+#define MYOPTIONAL_H
+
+enum class Status {
+    OK,
+    INVALID_ARG,
+    ERROR
+};
+
+
+#endif /* !MYOPTIONAL_H */
---
+++ test/myoptional_test.cpp
@@ -0,0 +1,10 @@
+#include <myoptional.h>
+#include <cstdlib>
+
+int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
+
+
+
